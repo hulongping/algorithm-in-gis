a5ccc7000588891bc808883799e8a867332e711a
harveyhu <lphu00@126.com>
2017-04-16 12:59:04 +0800

    初始化项目仓库

---
+++ README.md
@@ -0,0 +1,5 @@
+# 本项目用于学习记录一些常用的数据结构算法
+
+## Dijkstra算法
+常用语GIS中的求解有向图的最短路径
+com.harvey.experiment.dijkstra有实现算法
---
+++ src/com/harvey/experiment/dijkstra/Vertex.java
@@ -0,0 +1,7 @@
+package com.harvey.experiment.dijkstra;
+
+/**
+ * Created by harveyhu on 2017/4/16.
+ */
+public class Vertex {
+}
